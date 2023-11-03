174e077264bc6d94259ad868c470e032b407605f
ShekarArun <shekar.arun@gmail.com>
2023-11-03 15:44:26 +0530

    feat: Add paths with common roots

--- src/main.py
+++ src/main.py
@@ -8,6 +8,13 @@ async def root():
     return {"message": "Welcome to the library management system!"}
 
 
+@app.get("/books/all-isbn")
+async def get_isbn():
+    return {"isbn_list": [111, 123, 333, 543]}
+
+
+# Multiple paths can be used with same root
+# Paths are executed in sequence, so if the above paths are not matched, this function is executed
 @app.get("/books/{isbn}")
 async def get_isbn(isbn: int):
     return {"isbn": isbn}
